d52ee617dd7e7942044183c96e365608f5f9f96d
desertedeagle8383 <desertedeagle8383@gmail.com>
2015-04-08 10:24:24 -0400

    Update Enemy.java

--- Enemy.java
+++ Enemy.java
@@ -3,20 +3,22 @@ import java.util.ArrayList;
 public class Enemy extends Plane{
 	private long initialTime;
 	private int health;
+	private int width;
+	private int height;
 	private ArrayList<Action> actions;
 	
-	public Enemy(int health, ArrayList<Action> actions){
+	public Enemy(int width, int height, int health, ArrayList<Action> actions){
 		this.health = health;
 		this.actions = actions;
+		this.width = width;
+		this.height = height;
 	}
 	
 	public ArrayList<Action> getActions(){return actions;}
+	public int getWidth() {return width;}
+	public int getHeight() {return height;}
 	public long getInitialTime(){return initialTime;}
 	public void setInitialTime(long initialTime){this.initialTime = initialTime;}
 	public int getHealth(){return health;}
-	public boolean isAlive(){
-		if (health == 0)
-			return false;
-		return true;
-	}
+	public boolean isAlive(){return (health == 0);}
 }
