cc5c37327e113a75fa8530be0af97b7225ca1478
desertedeagle8383 <desertedeagle8383@gmail.com>
2015-04-08 10:26:28 -0400

    Update Thing.java

--- Thing.java
+++ Thing.java
@@ -7,14 +7,17 @@ public abstract class Thing {
 	private Coordinate currentCoordinate;
 	private double xVelocity;
 	private double yVelocity;
-	Image image;
+	private Image image;
 
-	
 	public Coordinate getCoordinate(){return currentCoordinate;};
+	public void setCoordinate(Coordinate newCoordinate) {
+		currentCoordinate = newCoordinate;
+	}
 	public double getXVelocity(){return xVelocity;};
 	public double getYVelocity(){return yVelocity;};
 	public void setXVelocity(double newXVelocity){xVelocity = newXVelocity;}
 	public void setYVelocity(double newYVelocity){yVelocity = newYVelocity;}
+	//Implement sprites
 	public void setImage(String filePath){
 		try {
 			image = ImageIO.read(new File(filePath));
