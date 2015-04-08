d655b8fdbede17ec5d543761212de5974050dc87
desertedeagle8383 <desertedeagle8383@gmail.com>
2015-04-08 10:23:34 -0400

    Update Action.java

--- Action.java
+++ Action.java
@@ -3,13 +3,17 @@ public class Action {
 	private double xVelocity;
 	private double yVelocity;
 	private boolean fire;
+	private boolean loop;
 	private long delay;
+	private long loopDelay;
 
-	public Action(double dx, double dy, boolean shoot, long time) {
+	public Action(double dx, double dy, boolean shoot, boolean repeat, long time, long repeatDelay) {
 		xVelocity = dx;
 		yVelocity = dy;
 		fire = shoot;
 		delay = time;
+		loop = repeat;
+		loopDelay = repeatDelay;
 	}
 	public double getXVelocity() {
 		return xVelocity;
@@ -22,5 +26,11 @@ public class Action {
 	}
 	public long getDelay() {
 		return delay;
+	}
+	public boolean getLoop() {
+		return loop;
+	}
+	public long getLoopDelay() {
+		return loopDelay;
 	}
 }
