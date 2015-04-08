52ab61b0f81aec31c1745a9e9d0d4e57d43b034f
desertedeagle8383 <desertedeagle8383@gmail.com>
2015-04-08 09:41:21 -0400

    Update GameFrame.java

--- GameFrame.java
+++ GameFrame.java
@@ -34,22 +34,16 @@ public class GameFrame extends JFrame implements KeyListener{
 	public void keyPressed(KeyEvent e) {
 		if (e.getKeyCode() == KeyEvent.VK_LEFT){
 			game.getGrid().getPlayer().setXVelocity(-1);
-		} 
-		if (e.getKeyCode() == KeyEvent.VK_RIGHT){
+		} else if (e.getKeyCode() == KeyEvent.VK_RIGHT){
 			game.getGrid().getPlayer().setXVelocity(-1);
-		}
-		if (e.getKeyCode() == KeyEvent.VK_UP){
+		} else if (e.getKeyCode() == KeyEvent.VK_UP){
 			game.getGrid().getPlayer().setYVelocity(1);
-		}
-		if (e.getKeyCode() == KeyEvent.VK_DOWN){
+		} else if (e.getKeyCode() == KeyEvent.VK_DOWN){
 			game.getGrid().getPlayer().setYVelocity(-1);
-		} 
-		if (e.getKeyCode() == KeyEvent.VK_SPACE){
+		} else if (e.getKeyCode() == KeyEvent.VK_SPACE){
 			game.getGrid().getPlayer().fire();
 		}
-
-}
-
-public void keyReleased(KeyEvent arg0) {}
-public void keyTyped(KeyEvent arg0) {}
+	}
+	public void keyReleased(KeyEvent arg0) {}
+	public void keyTyped(KeyEvent arg0) {}
 }
