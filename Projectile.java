7db1c753dc00084d5e9869279e42c9d185cfc21b
desertedeagle8383 <desertedeagle8383@gmail.com>
2015-04-08 10:26:16 -0400

    Update Projectile.java

--- Projectile.java
+++ Projectile.java
@@ -1,4 +1,12 @@
 
 public class Projectile extends Thing{
-
+	private boolean enemyProjectile;
+	
+	public Projectile(boolean enemy, Coordinate coordinate) {
+		enemyProjectile = enemy;
+		this.setCoordinate(coordinate);
+	}
+	public boolean isEnemyProjectile() {
+		return enemyProjectile;
+	}
 }
