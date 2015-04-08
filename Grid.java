e89d7f9573029d64fe5bdddd8b0003851cb986bd
desertedeagle8383 <desertedeagle8383@gmail.com>
2015-04-08 10:25:24 -0400

    Update Grid.java

--- Grid.java
+++ Grid.java
@@ -2,33 +2,33 @@ import java.util.ArrayList;
 
 public class Grid {
 	private int width;
-    private int height;
-    private ArrayList<Enemy> enemies;
-    private ArrayList<Projectile> projectiles;
-    private Player player;
-    
-    public Grid(int width, int height){
-    	this.width = width;
-    	this.height = height;
-    }
-    public void addEnemy(Enemy enemy){
-    	enemies.add(enemy);
-    }
-    public void removeEnemy(Enemy enemy){
-    	enemies.remove(enemy);
-    }
-    public void addProjectile(Projectile projectile){
-    	projectiles.add(projectile);
-    }
-    public void removeProjectile(Projectile projectile){
-    	projectiles.remove(projectile);
-    }
-    public int getWidth(){return width;}
-    public int getHeight(){return height;}
-    public ArrayList<Enemy> getEnemies(){return enemies;}
-    public ArrayList<Projectile> getProjectiles(){return projectiles;}
-    public void createPlayer(){
-    	player = new Player();
-    }
-    public Player getPlayer(){return player;}
+	private int height;
+	private ArrayList<Enemy> enemies;
+	private ArrayList<Projectile> projectiles;
+	private Player player;
+
+	public Grid(int width, int height){
+		this.width = width;
+		this.height = height;
+	}
+	public void addEnemy(Enemy enemy){
+		enemies.add(enemy);
+	}
+	public void removeEnemy(Enemy enemy){
+		enemies.remove(enemy);
+	}
+	public void addProjectile(Projectile projectile){
+		projectiles.add(projectile);
+	}
+	public void removeProjectile(Projectile projectile){
+		projectiles.remove(projectile);
+	}
+	public int getWidth(){return width;}
+	public int getHeight(){return height;}
+	public ArrayList<Enemy> getEnemies(){return enemies;}
+	public ArrayList<Projectile> getProjectiles(){return projectiles;}
+	public void createPlayer(){
+		player = new Player();
+	}
+	public Player getPlayer(){return player;}
 }
