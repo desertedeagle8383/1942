1ad5998ddfe41a1ec4c4b64e68c73f71d03d2702
desertedeagle8383 <desertedeagle8383@gmail.com>
2015-04-10 10:09:28 -0400

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,10 +1,11 @@
 import java.awt.event.ActionListener;
+import java.sql.Date;
 import java.util.ArrayList;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class Game{
-	private int currentLevel = 1;
+	private int currentLevel;
 	private int score;
 	private int lives;
 	private int rolls;
@@ -33,8 +34,24 @@ public class Game{
 		grid.addProjectile(newProjectile);
 	}
 
-	public void nextFrame(){
-		//what to do here?
+	public void nextFrame(long initialTime){
+		long elapsedTime = System.currentTimeMillis() - initialTime;
+		
+		for (int i = 0; i < grid.getEnemies().size(); i++) {
+			Enemy currentEnemy = grid.getEnemies().get(i);
+			Coordinate lastCoo = currentEnemy.getCoordinate();
+			for(int j = 0; j < currentEnemy.getActions().size(); i++) {
+				Action currentAction = currentEnemy.getActions().get(i);
+				long delay = currentAction.getDelay();
+				if (delay < elapsedTime && delay > elapsedTime - 30) {
+					currentEnemy.setXVelocity(currentAction.getXVelocity());
+					currentEnemy.setYVelocity(currentAction.getYVelocity());
+					if (currentAction.getFire())
+						grid.addProjectile()
+				}
+			}
+			currentEnemy.setCoordinate(new Coordinate((int) (lastCoo.getX() + currentEnemy.getXVelocity()), (int) (lastCoo.getY() + currentEnemy.getYVelocity())));
+		}
 	}
 	
 	public void startLevel(){
@@ -45,22 +62,28 @@ public class Game{
 				public void run() {
 					endLevel();
 				}
-			}, levels.get(currentLevel).getLevelLength());
+			}, new Date(initialTime + levels.get(currentLevel).getLevelLength()));
+			timer.scheduleAtFixedRate(new TimerTask() {
+				public void run() {
+					nextFrame(initialTime);
+				}
+			}, new Date(initialTime + 3000), 30);
 		} else {
 			endGame();
 		}
 	}
 	
 	public void startGame() {
-		
+		currentLevel = 1;
+		startLevel();
 	}
 	
 	private void endLevel() {
-		
+		cancelTasks();
 	}
 	
 	private void endGame() {
-		
+		cancelTasks();
 	}
 
 	public void cancelTasks(){
