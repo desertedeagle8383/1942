5fc16ce5d441dc8e1b927ab50e11241a425698b0
desertedeagle8383 <desertedeagle8383@gmail.com>
2015-04-13 09:45:49 -0400

    Update Thing.java

--- Thing.java
+++ Thing.java
@@ -5,18 +5,18 @@ import javax.imageio.ImageIO;
 
 public abstract class Thing {
 	private Coordinate currentCoordinate;
-	private double xVelocity;
-	private double yVelocity;
+	private int xVelocity;
+	private int yVelocity;
 	private Image image;
 
 	public Coordinate getCoordinate(){return currentCoordinate;};
 	public void setCoordinate(Coordinate newCoordinate) {
 		currentCoordinate = newCoordinate;
 	}
-	public double getXVelocity(){return xVelocity;};
-	public double getYVelocity(){return yVelocity;};
-	public void setXVelocity(double newXVelocity){xVelocity = newXVelocity;}
-	public void setYVelocity(double newYVelocity){yVelocity = newYVelocity;}
+	public int getXVelocity(){return xVelocity;};
+	public int getYVelocity(){return yVelocity;};
+	public void setXVelocity(int newXVelocity){xVelocity = newXVelocity;}
+	public void setYVelocity(int newYVelocity){yVelocity = newYVelocity;}
 	//Implement sprites
 	public void setImage(String filePath){
 		try {
