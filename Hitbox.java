9fad6cc7758f2f929060973562b4139226d9b1a3
desertedeagle8383 <desertedeagle8383@gmail.com>
2015-04-13 10:09:39 -0400

    Update Hitbox.java

--- Hitbox.java
+++ Hitbox.java
@@ -1,37 +1,41 @@
 import java.awt.Rectangle;
 
 public class Hitbox {
-	public boolean hit(Player player, Projectile projectile){
-		// size of player image -> 30 x 30 pixels, size of projectile -> 10 x 10
-		
-		Rectangle playerBox = new Rectangle();
-		Rectangle projectileBox = new Rectangle();
-		
-		playerBox.setBounds(player.getCoordinate().getX(), player.getCoordinate().getY(), 30, 30);
-		projectileBox.setBounds(projectile.getCoordinate().getX(), projectile.getCoordinate().getY(), 5, 5);
-		
-		return playerBox.intersects(projectileBox);
+	private Coordinate coordinate;
+	private final int width;
+	private final int height;
+	public Hitbox (Coordinate coo, int width, int height) {
+		coordinate = coo;
+		this.width = width;
+		this.height = height;
 	}
-	public boolean hit(Enemy enemy, Projectile projectile){
+	
+	public void setCoordinate(Coordinate coo) {
+		coordinate = coo;
+	}
+	
+	public Coordinate getCoordinate() {
+		return coordinate;
+	}
+	
+	public int getWidth() {
+		return width;
+	}
+	
+	public int getHeight() {
+		return height;
+	}
+	
+	private Rectangle getRectangle() {
+		return new Rectangle(coordinate.getX(), coordinate.getY(), width, height);
+	}
+	
+	public boolean hit(Hitbox hitbox){
 		// size of enemy image -> 30 x 30 pixels, size of projectile -> 10 x 10
 		
-		Rectangle enemyBox = new Rectangle();
-		Rectangle projectileBox = new Rectangle();
-		
-		enemyBox.setBounds(enemy.getCoordinate().getX(), enemy.getCoordinate().getY(), enemy.getWidth(), enemy.getHeight());
-		projectileBox.setBounds(projectile.getCoordinate().getX(), projectile.getCoordinate().getY(), 5, 5);
-		
-		return enemyBox.intersects(projectileBox);
-	}
-	public boolean hit(Player player, Enemy enemy){
-		// size of enemy image -> 30 x 30 pixels, size of projectile -> 10 x 10
-		
-		Rectangle playerBox = new Rectangle();
-		Rectangle enemyBox = new Rectangle();
+		Rectangle box1 = getRectangle();
+		Rectangle box2 = new Rectangle(hitbox.getCoordinate().getX(), hitbox.getCoordinate().getY(), hitbox.getWidth(), hitbox.getHeight());
 
-		playerBox.setBounds(player.getCoordinate().getX(), player.getCoordinate().getY(), 30, 30);
-		enemyBox.setBounds(enemy.getCoordinate().getX(), enemy.getCoordinate().getY(), enemy.getWidth(), enemy.getHeight());
-
-		return playerBox.intersects(enemyBox);
+		return box1.intersects(box2);
 	}
 }
