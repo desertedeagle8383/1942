ebbefb05ba2233d5a9ce8f3b9a5ea5d89e3a5ca4
dengcongdi <dengcongdi@yahoo.com>
2015-04-15 10:21:53 -0400

    Update GameFrame.java

--- GameFrame.java
+++ GameFrame.java
@@ -3,46 +3,45 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+
+import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-
 public class GameFrame extends JFrame{
 	private Game game;
-	private GamePanel panel;
+	private GamePanel gamePanel;
 	private JLabel scoreLabel, livesLabel, rollsLabel;
-	private JPanel contentPane;	
-	
+	private JPanel contentPane, labels;
 	public GameFrame(Game game){
 		this.game = game;
+				
+		contentPane = new JPanel();
+		setContentPane(contentPane);
+		
+		gamePanel = new GamePanel(game.getGrid());
+		gamePanel.addKeyListener(new GameKeyListener());
+		gamePanel.setFocusable(true);
+		labels = new JPanel();
+		
+		gamePanel.setPreferredSize(new Dimension(800, 750));
+		labels.setPreferredSize(new Dimension (800, 50));
+		
+		contentPane.add(labels);
+		contentPane.add(gamePanel);
 		
 		scoreLabel = new JLabel(Integer.toString(game.getScore()));
 		livesLabel = new JLabel(Integer.toString(game.getLives()));
 		rollsLabel = new JLabel(Integer.toString(game.getRolls()));
-		
-		game.getGrid().createPlayer();
-		
-		panel = new GamePanel(game.getGrid());
-		panel.addKeyListener(new GameKeyListener());
-		panel.setFocusable(true);
-		
-		contentPane = new JPanel();
+		labels.add(scoreLabel);
+		labels.add(livesLabel);
+		labels.add(rollsLabel);
+
+		setSize(800, 800);
+		setVisible(true);
 		
 	}
-
 	public void initializeGUI(){
-		setSize(800, 800);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setContentPane(contentPane);
-		add(panel);
-		
-		contentPane.setSize(new Dimension(800, 50));
-		contentPane.add(scoreLabel);
-		contentPane.add(livesLabel);
-		
-		panel.setSize(new Dimension(800, 750));
-		
-		setVisible(true);
 		
 	}
 	public void updateLabels(){
@@ -63,7 +62,6 @@ public class GameFrame extends JFrame{
 			} else if (e.getKeyCode() == KeyEvent.VK_SPACE){
 				game.shoot();
 			}
-			
 		}
 		public void keyReleased(KeyEvent e) {
 			if (e.getKeyCode() == KeyEvent.VK_LEFT){
@@ -78,15 +76,11 @@ public class GameFrame extends JFrame{
 		}
 		public void keyTyped(KeyEvent arg0) {}
 	}
-	public void paintComponent(Graphics g){
-		super.paintComponents(g);
-		panel.updatePanel(g);
 
-	}
 	public static void main(String args[]){
 		Grid grid = new Grid(800, 750);
 		Game game = new Game(grid);
 		GameFrame frame = new GameFrame(game);
-		frame.initializeGUI();
+	
 	}
 }
