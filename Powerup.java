aeb448a41cfab0c4cd4170e0f0e02eaa20f2fa9d
desertedeagle8383 <desertedeagle8383@gmail.com>
2015-04-15 10:24:40 -0400

    Update Powerup.java

--- Powerup.java
+++ Powerup.java
@@ -1,13 +1,26 @@
 import java.awt.Image;
 
-public abstract class Powerup extends Thing{
+public class Powerup extends Thing{
+	public static final int QUAD_GUN = 314;
+	public static final int ENEMY_CRASH = 159;
+	public static final int EXTRA_LIFE = 265;
+	public static final int NO_ENEMY_BULLETS = 358;
+	public static final int EXTRA_LOOP = 979;
+	public static final int THOUSAND_POINTS = 323;
 	
-	public Powerup(Coordinate coordinate, Hitbox hitbox){
+	private int type;
+	
+	public Powerup(Coordinate coordinate, Hitbox hitbox, int powerupType){
+		type = powerupType;
 		setHitbox(new Hitbox(coordinate, 20, 20));
 		setCoordinate(coordinate);
 		setXVelocity(0);
 		setYVelocity(5);
 		setWidth(20);
 		setHeight(20);
+	}
+	
+	public int getType() {
+		return type;
 	}
 }
