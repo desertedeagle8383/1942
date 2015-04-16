7618888070746d3f1184e233d271123583309fdf
dengcongdi <dengcongdi@yahoo.com>
2015-04-16 09:14:15 -0400

    Update GamePanel.java

--- GamePanel.java
+++ GamePanel.java
@@ -1,32 +1,19 @@
 import java.awt.*;
 import javax.swing.JPanel;
-
 public class GamePanel extends JPanel{
 	private Grid grid;
-	
 	public GamePanel(Grid grid){
 		this.grid = grid;
 	}
 	
-	public void updatePanel(Graphics g){
+	protected void paintComponent(Graphics g){
 		super.paintComponent(g);
-		Graphics2D g2 = (Graphics2D) g;
 		
-		for(int i = 0; i < grid.getEnemies().size(); i++)
-			g2.drawImage(grid.getEnemies().get(i).getImage(), 
-			grid.getEnemies().get(i).getCoordinate().getX(), 
-			grid.getEnemies().get(i).getCoordinate().getY(), 
-			null); 
-		
-		for(int i = 0; i < grid.getProjectiles().size(); i++)
-			g2.drawImage(grid.getProjectiles().get(i).getImage(), 
-			grid.getProjectiles().get(i).getCoordinate().getX(), 
-			grid.getProjectiles().get(i).getCoordinate().getY(), 
-			null); 
-		
-		g2.drawImage(grid.getPlayer().getImage(), 
-		grid.getPlayer().getCoordinate().getX(), 
-		grid.getPlayer().getCoordinate().getY(), 
-		null);
+		Graphics2D g2 = (Graphics2D)g;
+		g2.drawImage(grid.getPlayer().getImage(), grid.getPlayer().getCoordinate().getX() + grid.getPlayer().getXVelocity(), grid.getPlayer().getCoordinate().getY() + grid.getPlayer().getYVelocity(), grid.getPlayer().getWidth(), grid.getPlayer().getHeight(), null);
+	}
+	
+	public void updatePanel(){
+		repaint();
 	}
 }
