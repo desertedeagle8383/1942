bc1cdf3704c2992bdb9e8cf4db8e1bc8a1057578
desertedeagle8383 <desertedeagle8383@gmail.com>
2015-04-21 10:08:57 -0400

    Update Coordinate.java

--- Coordinate.java
+++ Coordinate.java
@@ -9,4 +9,8 @@ public class Coordinate {
 	public int getY(){return (int)y;}
 	public void setX(int newX){x = newX;}
 	public void setY(int newY){y = newY;}
+	
+	public String toString() {
+		return ("(" + x + ", " + y + ")");
+	}
 }
