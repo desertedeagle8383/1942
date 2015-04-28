edf5cda40e4549d6ec272c2039a6467315747e4f
desertedeagle8383 <desertedeagle8383@gmail.com>
2015-04-28 10:10:03 -0400

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,15 +1,23 @@
 public class Player extends Thing{
 	private boolean hittable;
-	private String filePath = "Z:\\My Documents\\1942\\src\\Player.png";
+	private String filePath1 = "Z:\\programming\\workspace2\\1942\\src\\Player.png";
+	private String filePath2 = "Z:\\programming\\workspace2\\1942\\src\\Shielded_Player.png";
 	public Player() {
+		hittable = true;
 		setCoordinate(new Coordinate(400, 100));
 		setXVelocity(0);
 		setYVelocity(0);
 		setWidth(30);
 		setHeight(30);
 		setHitbox(new Hitbox(new Coordinate(400, 100), 30, 30));
-		setImage(filePath);
+		setImage(filePath1);
 	}
 	public boolean isHittable(){return hittable;}
-	public void setHittable(boolean hittable){this.hittable = hittable;}
+	public void setHittable(boolean hittable){
+		this.hittable = hittable;
+		if (hittable) 
+			setImage(filePath1);
+		else
+			setImage(filePath2);
+	}
 }
