381c682497197a781871b5b0bf91145961e18883
desertedeagle8383 <desertedeagle8383@gmail.com>
2015-05-04 10:08:46 -0400

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,23 +1,46 @@
-public class Player extends Thing{
-	private boolean hittable;
-	private String filePath1 = "Player.png";
-	private String filePath2 = "Shielded_Player.png";
-	public Player() {
-		hittable = true;
-		setCoordinate(new Coordinate(400, 100));
+import java.util.ArrayList;
+
+public class Enemy extends Thing {
+	private long initialTime;
+	private int health;
+	private int powerup;
+	private ArrayList<Action> actions;
+	private int points;
+	private String filePath = "Enemy.png";
+	public Enemy(Coordinate coordinate, int width, int height, int initX, int initY, int health, int points, ArrayList<Action> actions){
+		this.health = health;
+		this.actions = actions;
+		this.powerup = 0;
+		this.points = points;
+		setXVelocity(initX);
+		setYVelocity(initY);
+		setCoordinate(coordinate);
+		setHitbox(new Hitbox(coordinate, width, height));
+		setWidth(width);
+		setHeight(height);
+
+	}
+	public Enemy(Coordinate coordinate, int width, int height, int initX, int initY, int health, int points, ArrayList<Action> actions, int powerupType){
+		this.health = health;
+		this.actions = actions;
+		this.powerup = powerupType;
+		this.points = points;
+		setCoordinate(coordinate);
+		setHitbox(new Hitbox(coordinate, width, height));
+		setWidth(width);
+		setHeight(height);
 		setXVelocity(0);
 		setYVelocity(0);
-		setWidth(30);
-		setHeight(30);
-		setHitbox(new Hitbox(new Coordinate(400, 100), 30, 30));
-		setImage(filePath1);
+		setImage(filePath);
 	}
-	public boolean isHittable(){return hittable;}
-	public void setHittable(boolean hittable){
-		this.hittable = hittable;
-		if (hittable) 
-			setImage(filePath1);
-		else
-			setImage(filePath2);
-	}
+
+	public int getPoints() {return points;}
+	public ArrayList<Action> getActions(){return actions;}
+	public long getInitialTime(){return initialTime;}
+	public void setInitialTime(long initialTime){this.initialTime = initialTime;}
+	public int getHealth(){return health;}
+	public boolean hasPowerup() {return powerup != 0;}
+	public Powerup getPowerup() {return new Powerup(getCoordinate(), powerup);}
+	public boolean isAlive(){return (health > 0);}
+	public void hit() {health--;}
 }
