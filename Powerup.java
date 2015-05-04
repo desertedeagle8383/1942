4486ccd25f59ee37c2627013acb3f379ae0cd680
desertedeagle8383 <desertedeagle8383@gmail.com>
2015-05-04 10:08:14 -0400

    Update Powerup.java

--- Powerup.java
+++ Powerup.java
@@ -6,16 +6,31 @@ public class Powerup extends Thing{
 	public static final int EXTRA_SHIELD = 979;
 	public static final int THOUSAND_POINTS = 323;
 	
+	private String filePath1 = "Quad_Gun.png";
+	private String filePath2 = "Enemy_Crash.png";
+	private String filePath3 = "Extra_Life.png";
+	private String filePath4 = "No_Enemy_Bullets.png";
+	private String filePath5 = "Extra_Shield.png";
+	private String filePath6 = "Thousand_Points.png";
+	
 	private int type;
 	
-	public Powerup(Coordinate coordinate, Hitbox hitbox, int powerupType){
+	public Powerup(Coordinate coordinate, int powerupType){
 		type = powerupType;
 		setHitbox(new Hitbox(coordinate, 20, 20));
 		setCoordinate(coordinate);
 		setXVelocity(0);
-		setYVelocity(5);
+		setYVelocity(-5);
 		setWidth(20);
 		setHeight(20);
+		switch (powerupType) {
+		case QUAD_GUN: setImage(filePath1); break;
+		case ENEMY_CRASH: setImage(filePath2); break;
+		case EXTRA_LIFE: setImage(filePath3); break;
+		case NO_ENEMY_BULLETS: setImage(filePath4); break;
+		case EXTRA_SHIELD: setImage(filePath5); break;
+		case THOUSAND_POINTS: setImage(filePath6); break;
+		}
 	}
 	
 	public int getType() {
