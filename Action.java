13b5431ba1c2b468ceea58aef3ff332d9bf4b188
desertedeagle8383 <desertedeagle8383@gmail.com>
2015-05-12 09:47:41 -0400

    Update Action.java

--- Action.java
+++ Action.java
@@ -2,6 +2,7 @@
 public class Action {
 	private int xVelocity;
 	private int yVelocity;
+	private double angle;
 	private boolean fire;
 	private boolean loop;
 	private boolean aimAtPlayer;
@@ -10,7 +11,7 @@ public class Action {
 	private long delay;
 	private long loopDelay;
 
-	public Action(long time, boolean changeV, int dx, int dy, boolean shoot, boolean fireAtPlayer, Coordinate targetCoo, boolean repeat, long repeatDelay) {
+	public Action(long time, boolean changeV, int dx, int dy, boolean shoot, boolean fireAtPlayer, Coordinate targetCoo, double a, boolean repeat, long repeatDelay) {
 		xVelocity = dx;
 		yVelocity = dy;
 		fire = shoot;
@@ -19,7 +20,67 @@ public class Action {
 		loopDelay = repeatDelay;
 		aimAtPlayer = fireAtPlayer;
 		target = targetCoo;
+		angle = a;
 		changeVelocity = changeV;
+	}
+	public Action(long time, int dx, int dy, boolean repeat, long repeatDelay) {
+		xVelocity = dx;
+		yVelocity = dy;
+		fire = false;
+		delay = time;
+		loop = repeat;
+		loopDelay = repeatDelay;
+		aimAtPlayer = false;
+		target = null;
+		angle = 0;
+		changeVelocity = true;
+	}
+	public Action(long time, boolean fireAtPlayer, Coordinate targetCoo, boolean repeat, long repeatDelay) {
+		xVelocity = 0;
+		yVelocity = 0;
+		fire = true;
+		delay = time;
+		loop = repeat;
+		loopDelay = repeatDelay;
+		aimAtPlayer = fireAtPlayer;
+		target = targetCoo;
+		angle = 0;
+		changeVelocity = false;
+	}
+	public Action(long time, boolean fireAtPlayer, double a, boolean repeat, long repeatDelay) {
+		xVelocity = 0;
+		yVelocity = 0;
+		fire = true;
+		delay = time;
+		loop = repeat;
+		loopDelay = repeatDelay;
+		aimAtPlayer = fireAtPlayer;
+		target = null;
+		angle = a;
+		changeVelocity = false;
+	}
+	public Action(long time, int dx, int dy, boolean fireAtPlayer, Coordinate targetCoo, boolean repeat, long repeatDelay) {
+		xVelocity = dx;
+		yVelocity = dy;
+		fire = true;
+		delay = time;
+		loop = repeat;
+		loopDelay = repeatDelay;
+		aimAtPlayer = fireAtPlayer;
+		target = targetCoo;
+		changeVelocity = true;
+	}
+	public Action(long time, int dx, int dy, boolean fireAtPlayer, double a, boolean repeat, long repeatDelay) {
+		xVelocity = dx;
+		yVelocity = dy;
+		fire = true;
+		delay = time;
+		loop = repeat;
+		loopDelay = repeatDelay;
+		aimAtPlayer = fireAtPlayer;
+		target = null;
+		angle = a;
+		changeVelocity = true;
 	}
 	public int getXVelocity() {
 		return xVelocity;
@@ -45,10 +106,16 @@ public class Action {
 	public boolean changesVelocity() {
 		return changeVelocity;
 	}
+	public boolean hasTarget() {
+		return target != null;
+	}
 	public Coordinate getTargetCoordinate() {
 		return target;
 	}
+	public double getAngle() {
+		return angle;
+	}
 	public Action generateLoopedCopy() {
-		return new Action(delay+loopDelay, changeVelocity, xVelocity, yVelocity, fire, aimAtPlayer, target, true, loopDelay);
+		return new Action(delay+loopDelay, changeVelocity, xVelocity, yVelocity, fire, aimAtPlayer, target, angle, true, loopDelay);
 	}
 }
