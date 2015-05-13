cc88689ed6d3db184b5b971881cae8c0eff8e18a
desertedeagle8383 <desertedeagle8383@gmail.com>
2015-05-13 10:23:13 -0400

    Update Enemy.java

--- Enemy.java
+++ Enemy.java
@@ -3,14 +3,15 @@ import java.util.ArrayList;
 public class Enemy extends Thing {
 	private long initialTime;
 	private int health;
-	private Powerup powerup;
+	private int powerup;
 	private ArrayList<Action> actions;
 	private int points;
-	private String filePath = "Z:\\programming\\workspace2\\1942\\src\\Enemy.png";
+	private String filePath1 = "Enemy.png";
+	private String filePath2 = "Powerup_Enemy.png";
 	public Enemy(Coordinate coordinate, int width, int height, int initX, int initY, int health, int points, ArrayList<Action> actions){
 		this.health = health;
 		this.actions = actions;
-		this.powerup = null;
+		this.powerup = 0;
 		this.points = points;
 		setXVelocity(initX);
 		setYVelocity(initY);
@@ -18,36 +19,32 @@ public class Enemy extends Thing {
 		setHitbox(new Hitbox(coordinate, width, height));
 		setWidth(width);
 		setHeight(height);
-		setImage(filePath);
+		setImage(filePath1);
+
 	}
-	public Enemy(Coordinate coordinate, int width, int height, int health, int points, ArrayList<Action> actions, Powerup powerupDrop){
+	public Enemy(Coordinate coordinate, int width, int height, int initX, int initY, int health, int points, ArrayList<Action> actions, int powerupType){
 		this.health = health;
 		this.actions = actions;
-		this.powerup = powerupDrop;
+		this.powerup = powerupType;
 		this.points = points;
 		setCoordinate(coordinate);
 		setHitbox(new Hitbox(coordinate, width, height));
 		setWidth(width);
 		setHeight(height);
-		setXVelocity(0);
-		setYVelocity(0);
+		setXVelocity(initX);
+		setYVelocity(initY);
+		setImage(filePath2);
 	}
-	
+
 	public int getPoints() {return points;}
 	public ArrayList<Action> getActions(){return actions;}
 	public long getInitialTime(){return initialTime;}
 	public void setInitialTime(long initialTime){this.initialTime = initialTime;}
 	public int getHealth(){return health;}
-	public boolean hasPowerup() {return powerup == null;}
-	public Powerup getPowerup() {return powerup;}
+	public boolean hasPowerup() {return powerup != 0;}
+	public void removePowerup() {powerup = 0;}
+	public void removePoints() {points = 0;}
+	public Powerup getPowerup() {return new Powerup(getCoordinate(), powerup);}
 	public boolean isAlive(){return (health > 0);}
-	public int hit() {
-		health--;
-		if (isAlive()) {
-			return 0;
-		}
-		else {
-			return points;
-		}
-	}
+	public void hit() {health--;}
 }
